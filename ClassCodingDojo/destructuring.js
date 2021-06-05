a0246cb8c800176a92141bbe7a89f82092797dbd
Stefano <piero.quiroz93@gmail.com>
2021-06-05 17:20:18 -0500

    Destructuring class

---
+++ ClassCodingDojo/destructuring.js
@@ -0,0 +1,40 @@
+/* const person = { 
+    firstName: 'Bob', 
+    lastName: 'Marley', 
+    email: 'bob@marley.com', 
+    password: 'sekureP@ssw0rd9', 
+    username: 'barley', 
+    createdAt: 1543945177623
+};
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+
+const {firstName} = person;
+const [first] = animals;
+console.log({firstName, first}); */
+
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley',
+    email: 'bob@marley.com',
+    password: 'sekureP@ssw0rd9',
+    username: 'barley',
+    addresses: [
+      {
+        address: '1600 Pennsylvania Avenue',
+        city: 'Washington, D.C.',
+        zipcode: '20500',
+      },
+      {
+        address: '221B Baker St.',
+        city: 'London',
+        zipcode: 'WC2N 5DU',
+      }
+    ],
+    createdAt: 1543945177623
+  };
+
+/*   const {addresses: [whiteHouse, sherlock]} = person;
+  console.log([whiteHouse,sherlock]); */
+
+  const {addresses: [, {city}]} = person;
+  console.log(city)
